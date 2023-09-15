3dedff11a3c0276db7150fbe5116dd5d64c2d462
saurabhdaware <saurabhdaware99@gmail.com>
2023-09-15 23:50:14 +0530

    fix: js bundling

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-/**
- * You can write any of your client-side javascript here if needed
- */
+function toggleTheme(toggleButton) {
+  if (document.body.classList.contains('dark')) {
+    document.body.classList.remove('dark');
+    window.localStorage.setItem('prefers-theme', 'light');
+    if (toggleButton) {
+      toggleButton.setAttribute('aria-pressed', false);
+      toggleButton.setAttribute('aria-label', 'Activate Dark Mode');
+    }
+  } else {
+    document.body.classList.add('dark');
+    window.localStorage.setItem('prefers-theme', 'dark');
+    if (toggleButton) {
+      toggleButton.setAttribute('aria-pressed', true);
+      toggleButton.setAttribute('aria-label', 'Activate Light Mode');
+    }
+  }
+}
+
+document.querySelector('.dark-mode-toggle').addEventListener('click', (e) => {
+  toggleTheme(e.target);
+})
+
+const localPreference = window.localStorage.getItem('prefers-theme');
+
+if (localPreference) {
+  if (localPreference === 'light') document.body.classList.remove('dark');
+  else document.body.classList.add('dark');
+} else if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+  document.body.classList.add('dark');
+}
